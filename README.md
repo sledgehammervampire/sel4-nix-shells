e824dec28f0cc7fbf67bc28576a943bf313db362
Kevin Tran <47207223+1000teslas@users.noreply.github.com>
2022-01-27 20:54:26 +1100

    sel4 shell for x86 and arm

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # sel4-nix-shells
+## Usage
+First, make sure [Nix flakes are enabled](https://nixos.wiki/wiki/Flakes#:~:text=Installing%20flakes). Then type
+```
+nix develop $REPO_PATH#
+```
+into a shell, where `$REPO_PATH` is the path to this repo.
---
+++ flake.nix
@@ -0,0 +1,83 @@
+{
+  description = "sel4 development shells";
+
+  inputs = {
+    nixpkgs.url = github:nixos/nixpkgs/nixos-21.11;
+    mach-nix.url = github:DavHau/mach-nix;
+    flake-utils.url = github:numtide/flake-utils;
+  };
+
+  outputs =
+    { self
+    , nixpkgs
+    , mach-nix
+    , flake-utils
+    }:
+    flake-utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    rec {
+      packages = {
+        # package description ripped off from gcc-arm-embedded
+        gcc-arm-linux-gnueabi = pkgs.callPackage
+          ({ stdenv, fetchurl, lib, ncurses5, python27, expat }: stdenv.mkDerivation rec {
+            pname = "gcc-arm-linux-gnueabi";
+            version = "8.2";
+            release = "2019.01";
+            src = fetchurl
+              {
+                url = "https://developer.arm.com/-/media/Files/downloads/gnu-a/${version}-${release}/gcc-arm-${version}-${release}-x86_64-arm-linux-gnueabi.tar.xz";
+                sha256 = "sha256-OWUVAzGMMKqVXmO/gCxP7pzOWFdW0rPizdSkRX1AxFg=";
+              };
+            dontConfigure = true;
+            dontBuild = true;
+            dontPatchELF = true;
+            dontStrip = true;
+            installPhase = ''
+              mkdir -p $out
+              cp -r * $out
+              ln -s $out/share/man $out/man
+            '';
+            preFixup = ''
+              find $out -type f | while read f; do
+                patchelf "$f" > /dev/null 2>&1 || continue
+                patchelf --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) "$f" || true
+                patchelf --set-rpath ${lib.makeLibraryPath [ "$out" stdenv.cc.cc ncurses5 python27 expat ]} "$f" || true
+              done
+            '';
+            meta = { platforms = [ "x86_64-linux" ]; };
+          })
+          { };
+      };
+      devShell =
+        with pkgs;
+        mkShell
+          {
+            buildInputs =
+              let
+                myPython = (import mach-nix { inherit pkgs; python = "python39"; }).mkPython {
+                  requirements = ''
+                    sel4-deps
+                    setuptools
+                    protobuf==3.12.4
+                  '';
+                };
+              in
+              [
+                myPython
+                bashInteractive
+                gcc
+                ccache
+                cmake
+                ninja
+                libxml2
+                protobuf3_12
+                dtc
+                packages.gcc-arm-linux-gnueabi
+              ];
+          };
+    });
+}
