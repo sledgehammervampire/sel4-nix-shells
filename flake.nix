ce0ce35312cbcb4b76dc1969c2488006b3e66341
Kevin Tran <47207223+1000teslas@users.noreply.github.com>
2022-02-01 02:00:21 +1100

    override polyml for isabelle

--- flake.nix
+++ flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = github:nixos/nixpkgs/nixos-21.11;
     mach-nix.url = github:DavHau/mach-nix;
     flake-utils.url = github:numtide/flake-utils;
+    nixpkgs-master.url = github:nixos/nixpkgs/master;
   };
 
   outputs =
@@ -12,17 +13,36 @@
     , nixpkgs
     , mach-nix
     , flake-utils
+    , nixpkgs-master
     }:
     flake-utils.lib.eachDefaultSystem (system:
     let
       pkgs = import nixpkgs {
         inherit system;
       };
+      pkgs-master = import nixpkgs-master {
+        inherit system;
+      };
     in
     rec {
       packages = {
-        inherit (pkgs) isabelle;
-        # package description ripped off from gcc-arm-embedded
+        isabelle = with pkgs-master; callPackage ./isabelle.nix {
+          polyml = polyml.overrideAttrs (_: {
+            configureFlags = [ "--enable-intinf-as-int" "--with-gmp" "--disable-shared" ];
+            buildFlags = [ "compiler" ];
+            version = "for-isabelle-2021-1";
+            src = fetchFromGitHub {
+              owner = "polyml";
+              repo = "polyml";
+              rev = "39d96a2def903ed019c6855e3b688df5070d633a";
+              sha256 = "sha256-S7d2Vr/nB+rCX9d4qQj4f7edVZKocKIjc5rrx9A/B4Q=";
+            };
+          });
+
+          java = openjdk17;
+          z3 = z3_4_4_0;
+        };
+
         gcc-arm-linux-gnueabi = pkgs.callPackage ./gcc-arm-linux-gnueabi.nix { };
       };
       devShells = with pkgs;
